23763af0aa44d5cfd583dae20510b0903348a65e
Quazzik <Yuakorotkov@gmail.com>
2023-12-14 01:35:33 +0500

    Deleted comments

--- HexCutter.cpp
+++ HexCutter.cpp
@@ -16,27 +16,20 @@ ImgInfo* readPixelColorsFromFile(const std::string& filename) {
     vector<COLORREF>* pixelColors = new vector<COLORREF>();
 
     if (file.is_open()) {
-        // Чтение заголовка BMP файла
         char* header = new char[54];
         file.read(header, 54);
 
-        // Получение ширины и высоты изображения из заголовка
         int width = *(int*)&header[18];
         int height = *(int*)&header[22];
 
-        // Вычисление размера пиксельных данных
         int dataSize = width * height * 3;
 
-        // Выделение памяти для пиксельных данных
         char* data = new char[dataSize];
 
-        // Чтение пиксельных данных изображения
         file.read(data, dataSize);
 
-        // Определение цвета фона
         COLORREF backgroundColor = RGB((unsigned char)data[2], (unsigned char)data[1], (unsigned char)data[0]);
 
-        // Преобразование пиксельных данных в COLORREF и добавление их в вектор
         for (int i = 0; i < dataSize; i += 3) {
             COLORREF color = RGB((unsigned char)data[i + 2], (unsigned char)data[i + 1], (unsigned char)data[i]);
             pixelColors->push_back(color);
@@ -47,7 +40,6 @@ ImgInfo* readPixelColorsFromFile(const std::string& filename) {
 
         file.close();
 
-        // Замена цвета фона на ничего
         for (COLORREF& color : *pixelColors) {
             if (color == backgroundColor) {
                 color = RGB(255, 255, 255);
@@ -65,10 +57,8 @@ ImgInfo* readPixelColorsFromFile(const std::string& filename) {
 
 
 void drawShape(const ImgInfo* info, HDC hdcWindow, HWND hwnd) {
-    // Получение размеров окна
     auto pictWidth = info->width;
     auto pictHeight = info->height;
-
     auto pixelColors = info->pixels;
 
     RECT windowRect;
@@ -76,15 +66,9 @@ void drawShape(const ImgInfo* info, HDC hdcWindow, HWND hwnd) {
     LONG windowWidth = windowRect.right - windowRect.left;
     int windowHeight = windowRect.bottom - windowRect.top;
 
-    // Определение положения и размеров фигуры
-    /*int shapeLeft = windowWidth / 4;
-    int shapeTop = windowHeight / 4;
-    int shapeRight = windowWidth * 3 / 4;
-    int shapeBottom = windowHeight * 3 / 4;*/
-
     for (int x = 0; x < pictWidth; ++x) {
         for (int y = 0; y < pictHeight; ++y) {
-            SetPixel(hdcWindow, pictWidth - x + 100, y + 100, pixelColors->at(pixelColors->size() - (1 + pictWidth * y + x)));
+            SetPixel(hdcWindow, pictWidth - x + windowHeight/15, y + windowHeight - pictHeight - windowHeight/15, pixelColors->at(pixelColors->size() - (1 + pictWidth * y + x)));
         }
     }
 }
@@ -124,7 +108,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     HWND hwnd = CreateWindowExW(
         0,
         CLASS_NAME,
-        L"Та самая отрисовка которая не работает",
+        L"Та самая отрисовка которая уже работает",
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
         NULL,
